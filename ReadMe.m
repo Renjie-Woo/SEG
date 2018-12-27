79123eebc96c8599c32d7f936686e9e67d56a43a
Renjie-Woo <1569750165@qq.com>
2018-12-27 14:22:53 +0800

    Create ReadMe.m

---
+++ ReadMe.m
@@ -0,0 +1,10 @@
+## This folder is build for CG project "SEG-1" 
+## Team member: Renjie Wu, Xinyi Yu
+## Time: 2018/12/25
+## SJTU, CS 
+
+## The folder MySegNet is build for segmentation of Teeth
+## The folder Matlab is build for edge extractions
+## The folder PointCloud is build for 3D reconstruction by point clouds
+
+## SEG.png is the conference poster
